0ea312e173b6dca895d6945ab17d6339ef86c052
OstadTahmasb <arman.tahmasebizadeh@gmail.com>
2024-05-02 15:47:44 +0330

    Menus UML

---
+++ src/controller/PreGameMenu.java
@@ -0,0 +1,47 @@
+package controller;
+
+public class PreGameMenu {
+    public static void createGame(String playerUsername) {
+
+    }
+
+    public static void selectFaction(String faction) {
+
+    }
+
+    public static void saveDeckByAddress(String fileAddress) {
+
+    }
+
+    public static void saveDeckByName(String fileName) {
+
+    }
+
+    public static void loadDeckByAddress(String fileAddress) {
+
+    }
+
+    public static void loadDeckByName(String fileName) {
+
+    }
+
+    public static void selectLeader(String leaderName) {
+
+    }
+
+    public static void addToDeck(String cardName, String count) {
+
+    }
+
+    public static void deleteFromDeck(String cardName, String count) {
+
+    }
+
+    public static void changeTurn() {
+
+    }
+
+    public static void startGame() {
+
+    }
+}
---
+++ src/controller/ProfileMenu.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class ProfileMenu {
+    public static void changeUsername(String username) {
+
+    }
+
+    public static void changeNickname(String nickname) {
+
+    }
+
+    public static void changeEmail(String email) {
+
+    }
+
+    public static void changePassword(String newPassword, String oldPassword) {
+
+    }
+
+    
+}
