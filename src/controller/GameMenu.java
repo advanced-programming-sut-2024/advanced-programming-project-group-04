d2e624add8f1eb859fd702de9331c9305ee8edf2
Arman Tahmasebi <155746468+OstadTahmasb@users.noreply.github.com>
2024-05-02 17:05:19 +0330

    Arman (#1)
    * Menus UML
    
    * removing conflicts
    
    ---------
    
    Co-authored-by: OstadTahmasb <arman.tahmasebizadeh@gmail.com>

---
+++ src/controller/GameMenu.java
@@ -0,0 +1,77 @@
+package controller;
+
+import module.Card;
+import module.Leader;
+import module.Player;
+
+import java.util.ArrayList;
+
+public class GameMenu {
+    public static void vetoCard(String cardNumber) {
+
+    }
+
+    public static ArrayList<Card> inHandCard() {
+
+    }
+
+    public static Card inHandCard(String cardNumber) {
+
+    }
+
+    public static void getRemainingCardsToPlay() {
+
+    }
+
+    public static ArrayList<Card> getOutOfPlayCards() {
+
+    }
+
+    public static ArrayList<Card> getCardsInRow(String rowNumber) {
+
+    }
+
+    public static Card getSpellsInPlay() {
+
+    }
+
+    public static void placeCard(String cardNumber, String rowNumber) {
+
+    }
+
+    public static Leader getCommander() {
+
+    }
+
+    public static void commanderPowerPlay() {
+
+    }
+
+    public static Player getCurrentPlayer() {
+
+    }
+
+    public static Player getOpponent() {
+
+    }
+
+    public static int getCurrentPlayerLives() {
+
+    }
+
+    public static int getOpponentLives() {
+
+    }
+
+    public static int getTotalScore() {
+
+    }
+
+    public static int getRowTotalScore() {
+
+    }
+
+    public static void passRound() {
+
+    }
+}
---
+++ src/controller/LoginMenu.java
@@ -0,0 +1,28 @@
+package controller;
+
+public class LoginMenu {
+    public static void register(String username, String password, String passwordConfirm,
+                         String nickname, String email) {
+
+    }
+
+    public static void pickQuestion(String questionNumber, String answer, String answerConfirm) {
+
+    }
+
+    public static void login(String username, String password, boolean stayLoggedIn) {
+
+    }
+
+    public static void forgetPassword(String username) {
+
+    }
+
+    public static void answerQuestion(String questionNumber, String answer) {
+
+    }
+
+    public static void setPassword(String password) {
+
+    }
+}
