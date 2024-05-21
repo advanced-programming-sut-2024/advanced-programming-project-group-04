d07e76d9e1afbe0d41f428f690f083c50de3add7
OstadTahmasb <arman.tahmasebizadeh@gmail.com>
2024-05-21 14:00:52 +0330

    minor edits

--- src/model/card/AllCards.java
+++ src/model/card/AllCards.java
@@ -7,11 +7,12 @@ import static model.card.CardType.*;
 
 public enum AllCards {
     ARVIN("Arvin", -69, 85, GAY, new Berserker());
-    String name;
-    int power;
-    int number;
-    CardType type;
-    Ability ability;
+    final String name;
+    final int power;
+    final int number;
+    final CardType type;
+    final Ability ability;
+    
     AllCards(String name, int power, int number, CardType type, Ability ability) {
         this.name = name;
         this.power = power;
