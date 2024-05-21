4262a19500d2b6eb7dc81b7bc17285fd5a964bb2
Arman Tahmasebi <155746468+OstadTahmasb@users.noreply.github.com>
2024-05-21 18:52:01 +0330

    Changed unitcard system (#17)
    Co-authored-by: OstadTahmasb <arman.tahmasebizadeh@gmail.com>

--- src/model/card/specialcards/SpecialCard.java
+++
@@ -1,9 +0,0 @@
-package model.card.specialcards;
-import model.ability.Ability;
-import model.card.*;
-
-public class SpecialCard extends Card{
-    public SpecialCard (AllCards allCard) {
-        super(allCard);
-    }
-}
