e3388ebea097e1953db0d569978f8b84276a764a
OstadTahmasb <arman.tahmasebizadeh@gmail.com>
2024-06-21 15:30:52 +0330

    Fixing imports

--- core/src/com/mygdx/game/model/card/specialcards/SpecialCard.java
+++ core/src/com/mygdx/game/model/card/specialcards/SpecialCard.java
@@ -1,5 +1,4 @@
 package com.mygdx.game.model.card.specialcards;
-import com.mygdx.game.model.ability.Ability;
 import com.mygdx.game.model.card.*;
 
 public class SpecialCard extends Card{
--- core/src/com/mygdx/game/model/card/unitcards/UnitCard.java
+++ core/src/com/mygdx/game/model/card/unitcards/UnitCard.java
@@ -1,5 +1,4 @@
 package com.mygdx.game.model.card.unitcards;
-import com.mygdx.game.model.ability.Ability;
 import com.mygdx.game.model.card.*;
 
 public class UnitCard extends Card{
