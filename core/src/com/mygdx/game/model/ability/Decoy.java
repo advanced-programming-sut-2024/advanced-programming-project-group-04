d50a7ea92cb89f8907fbb0cbafe9733dd05ffb7c
Matin Ghiasi <153081884+bootna22@users.noreply.github.com>
2024-06-25 16:05:22 +0330

    Added  some of the abilities and left some TODO @Arvin comments for the methods to complete in game manager

--- core/src/com/mygdx/game/model/ability/Decoy.java
+++ core/src/com/mygdx/game/model/ability/Decoy.java
@@ -2,18 +2,14 @@ package com.mygdx.game.model.ability;
 
 import com.mygdx.game.model.GameManager;
 import com.mygdx.game.model.Position;
+import com.mygdx.game.model.card.AllCards;
 import com.mygdx.game.model.card.Card;
 
-import java.util.ArrayList;
-
-public class Mardroeme implements Ability {
+public class Decoy implements Ability {
     @Override
     public void run(GameManager gameManager, Card callerCard) {
         Position callerCardPosition = gameManager.findCardInGameForCurrentPlayer(callerCard);
-        ArrayList<Card> row = gameManager.getCardRowFromPosition(callerCardPosition);
-        for (Card card : row) {
-            if (card.isBerserker())
-                card.getAbility().run(gameManager, card);
-        }
+        gameManager.placeCard(new Card(AllCards.Decoy), callerCardPosition);
+        gameManager.removeCard(callerCard, callerCardPosition);
     }
 }
--- core/src/com/mygdx/game/model/ability/Scorch.java
+++ core/src/com/mygdx/game/model/ability/Scorch.java
@@ -1,11 +1,43 @@
 package com.mygdx.game.model.ability;
 
 import com.mygdx.game.model.GameManager;
+import com.mygdx.game.model.Position;
+import com.mygdx.game.model.card.AllCards;
 import com.mygdx.game.model.card.Card;
 
+import java.util.ArrayList;
+
 public class Scorch implements Ability {
+
     @Override
     public void run(GameManager gameManager , Card callerCard){
+        if (callerCard.getAllCard().equals(AllCards.Toad)) {
+            ArrayList<Card> cards = gameManager.getMaximumPowerInRow(Position.Range);
+            for (Card card : cards) {
+                gameManager.removeCard(card, Position.Range); //TODO @Arvin CHERA REMOVE CARD POSITION MIGIRE? OON KE YEKTAST JAYE FELISH
+            }
+        }
 
+        if (callerCard.getAllCard().equals(AllCards.Schirru)) {
+            ArrayList<Card> cards = gameManager.getMaximumPowerInRow(Position.Siege);
+            for (Card card : cards) {
+                gameManager.removeCard(card, Position.Siege); //TODO @Arvin CHERA REMOVE CARD POSITION MIGIRE? OON KE YEKTAST JAYE FELISH
+            }
+        }
+
+        if (callerCard.getAllCard().equals(AllCards.Villentretenmerth)) {
+            ArrayList<Card> cards = gameManager.getMaximumPowerInRow(Position.Melee);
+            for (Card card : cards) {
+                gameManager.removeCard(card, Position.Melee); //TODO @Arvin CHERA REMOVE CARD POSITION MIGIRE? OON KE YEKTAST JAYE FELISH
+            }
+        }
+
+        if (callerCard.getAllCard().equals(AllCards.Scorch) || callerCard.getAllCard().equals(AllCards.ClanDimunPirate)) {
+            ArrayList<Card> cards = gameManager.getMaximumPowerInField();
+            for (Card card : cards) {
+                Position cardPosition = gameManager.findCardInGameForCurrentPlayer(card);
+                gameManager.removeCard(card, cardPosition); //TODO @Arvin CHERA REMOVE CARD POSITION MIGIRE? OON KE YEKTAST JAYE FELISH
+            }
+        }
     }
 }
