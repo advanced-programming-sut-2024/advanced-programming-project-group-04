f2ab417becc88e1388fcc9a26503920ae3a6dbe8
arvnthr1384 <arvin.taheri12021384@gmail.com>
2024-06-25 20:47:15 +0330

    Factions Cards were added

--- core/src/com/mygdx/game/model/faction/Faction.java
+++ core/src/com/mygdx/game/model/faction/Faction.java
@@ -2,21 +2,47 @@ package com.mygdx.game.model.faction;
 
 import java.util.ArrayList;
 
-import static com.mygdx.game.model.card.AllCards.*;
 import com.mygdx.game.model.card.AllCards;
+import static com.mygdx.game.model.card.AllCards.*;
+
 import com.mygdx.game.model.leader.Leader;
 
 public class Faction {
     private final String name;
 
-    private final static ArrayList<AllCards> neutralCards = new ArrayList<>();
-    neutralCards = {
+    private static ArrayList<AllCards> neutralCards = new ArrayList<>();
 
-    };
     private ArrayList<Leader> leaders = new ArrayList<>();
+
+    static {
+        neutralCards.add(BitingFrost);
+        neutralCards.add(ImpenetrableFog);
+        neutralCards.add(TorrentialRain);
+        neutralCards.add(SkelligeStorm);
+        neutralCards.add(ClearWeather);
+        neutralCards.add(Scorch);
+        neutralCards.add(CommandersHorn);
+        neutralCards.add(Decoy);
+        neutralCards.add(Dandelion);
+        neutralCards.add(Cow);
+        neutralCards.add(EmielRegis);
+        neutralCards.add(GaunterODimm);
+        neutralCards.add(GaunterODimmDarkness);
+        neutralCards.add(GeraltOfRivia);
+        neutralCards.add(Avallach);
+        neutralCards.add(OlgierdVonEverc);
+        neutralCards.add(TrissMerigold);
+        neutralCards.add(Vesemir);
+        neutralCards.add(Villentretenmerth);
+        neutralCards.add(YenneferOfVengerberg);
+        neutralCards.add(ZoltanChivay);
+        neutralCards.add(BovineDefenseForce);
+        neutralCards.add(CirillaFionaElenRiannon);
+    }
 
     public Faction(String name) {
         this.name = name;
+        
     }
 
     public String getImageURL() {
--- core/src/com/mygdx/game/model/faction/Scoiatael.java
+++ core/src/com/mygdx/game/model/faction/Scoiatael.java
@@ -1,6 +1,39 @@
 package com.mygdx.game.model.faction;
 
+import java.util.ArrayList;
+
+import com.mygdx.game.model.card.AllCards;
+import static com.mygdx.game.model.card.AllCards.*;
+
 public class Scoiatael extends Faction{
+
+    private static ArrayList<AllCards> scoiataelCards = new ArrayList<>();
+
+    static {
+        scoiataelCards.add(ElvenSkirmisher);
+        scoiataelCards.add(Iorveth);
+        scoiataelCards.add(Yaevinn);
+        scoiataelCards.add(CiaranAepEasnillien);
+        scoiataelCards.add(DennisCranmer);
+        scoiataelCards.add(DolBlathannaScout);
+        scoiataelCards.add(DolBlathannaArcher);
+        scoiataelCards.add(DwarvenSkirmisher);
+        scoiataelCards.add(Filavandrel);
+        scoiataelCards.add(HavekarHealer);
+        scoiataelCards.add(HavekarSmuggler);
+        scoiataelCards.add(IdaEmeanAep);
+        scoiataelCards.add(Riordain);
+        scoiataelCards.add(Toruviel);
+        scoiataelCards.add(VriheddBrigadeRecruit);
+        scoiataelCards.add(MahakamanDefender);
+        scoiataelCards.add(VriheddBrigadeVeteran);
+        scoiataelCards.add(Milva);
+        scoiataelCards.add(Seasenthessis);
+        scoiataelCards.add(Schirru);
+        scoiataelCards.add(BarclayEls);
+        scoiataelCards.add(Eithne);
+        scoiataelCards.add(IsengrimFaoiltiarna);
+    }
     public Scoiatael() {
         super("Scoia’tael");
     }
