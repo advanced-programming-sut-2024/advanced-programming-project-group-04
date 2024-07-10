088057c541ef99230b3efd799962946a338c082f
arvnthr1384 <arvin.taheri12021384@gmail.com>
2024-07-10 12:42:13 +0330

    run leaders ok ta hadi

--- core/src/com/mygdx/game/view/GameMenu.java
+++ core/src/com/mygdx/game/view/GameMenu.java
@@ -705,6 +705,7 @@ public class GameMenu extends Menu implements CheatProcessor {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 // TODO @Arvin ina ro kamel kon ke vaghti leader click shod chi beshe
+                gameController.runLeader(gameController.getMyLeader());
             }
         });
 
